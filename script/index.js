f8a2a94014c6e753fe0bb740f5770c56ebdf0785
Gustavo <gustavo.souza.melo@hotmail.com>
2025-11-20 20:41:14 -0300

    correção bug console

--- script/index.js
+++ script/index.js
@@ -6,22 +6,3 @@ const navLinks = document.getElementById('nav-links');
 menuBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-
-// CARROSSEL
-let count = 1;
-document.getElementById("radio-servico1").checked = true;
-
-setInterval( function(){
-    nextImage();
-
-}, 5000)
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count=1;
-    }
-
-    document.getElementById("radio-servico"+count).checked = true;
-}
---
+++ script/scriptCarrossel.js
@@ -0,0 +1,16 @@
+let count = 1;
+document.getElementById("radio-servico1").checked = true;
+
+setInterval( function(){
+    nextImage();
+
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count=1;
+    }
+
+    document.getElementById("radio-servico"+count).checked = true;
+}
