608eb5e5211b6741b22876d53265086c342ab835
Gustavo <gustavo.souza.melo@hotmail.com>
2025-11-21 18:25:01 -0300

    correções gerais

--- script/index.js
+++ script/index.js
@@ -6,3 +6,20 @@ const navLinks = document.getElementById('nav-links');
 menuBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+//FADE IN SCRIPT
+    
+        const fadeElements = document.querySelectorAll(".fade-in");
+
+        function checkFade() {
+            fadeElements.forEach(el => {
+                const rect = el.getBoundingClientRect();
+                if (rect.top < window.innerHeight - 100) {
+                    el.classList.add("show");
+                }
+            });
+        }
+
+        window.addEventListener("scroll", checkFade);
+        window.addEventListener("load", checkFade);
