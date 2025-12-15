f8d97f993db7c153480a1366f2804f17e2ae365e
Gustavo <gustavo.souza.melo@hotmail.com>
2025-12-15 20:40:44 -0300

    removendo pdf do link de download e adicionando-o a um iframe

--- script/index.js
+++ script/index.js
@@ -24,6 +24,34 @@ function checkFade() {
 window.addEventListener("scroll", checkFade);
 window.addEventListener("load", checkFade);
 
+// ABRIR CATALOGO
+const openCatalogo = document.getElementById('open-catalogo');
+const modalCatalogo = document.getElementById('catalogo-modal');
+const iframeCatalogo = modalCatalogo.querySelector('iframe');
+const closeCatalogo = modalCatalogo.querySelector('.close-catalogo');
+
+openCatalogo.addEventListener('click', () => {
+    if (!iframeCatalogo.src) {
+        iframeCatalogo.src = './assets/catalogo/Catalogo_Oliver_Vidros.pdf';
+    }
+
+    modalCatalogo.classList.add('active');
+    document.body.style.overflow = 'hidden';
+});
+
+closeCatalogo.addEventListener('click', closeModal);
+
+modalCatalogo.addEventListener('click', (e) => {
+    if (e.target === modalCatalogo) {
+        closeModal();
+    }
+});
+
+function closeModal() {
+    modalCatalogo.classList.remove('active');
+    document.body.style.overflow = '';
+}
+
 // FORMULARIO EMAIL JS
 emailjs.init("NXLW-n0BeYcoaDqI7");
 
